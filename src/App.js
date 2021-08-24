c3e7e7bdab597205195cfc2cb4715830615a14ea
Avetis Avagyan <avikavagyan2002@gmail.com>
2021-08-24 02:53:06 -0400

    Created "index.css", added Import of "index.css", and wrote first header.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
+// App only returns Welcome Page so far
 function App() {
-  return ( <div> hello </div> );
+  return (
+    <div> 
+      <h1> Welcome to QuickType </h1> 
+    </div>
+  );
 }
 
 export default App;
