1f7e4207225662b64e459957f40549ad3dea373a
Kefan Cao <k33cao@uwaterloo.ca>
2021-08-24 21:43:19 -0400

    Added the word component

---
+++ src/Word.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './word.css'
+
+export default function Word(props) {
+	const letters = props.inputWord.split(''); 
+	return (
+		<div className="word">
+			{letters.map( char => <letter>{char}</letter>)}
+		</div>
+	)
+}
