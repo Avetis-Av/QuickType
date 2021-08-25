4c22cd5e5e4033b615de9ada1f07211c5dc8960b
Kefan Cao <k33cao@uwaterloo.ca>
2021-08-25 17:15:47 -0400

    Added dynamic styling

--- src/typeTest.js
+++ src/typeTest.js
@@ -1,15 +1,45 @@
-import React from 'react'; 
+import React, {useState, useEffect} from 'react'; 
 import Word from './word'; 
-import './typeTest.css'
+import './typeTest.css' 
 
 export default function TypeTest() {
-	const test1 = "the quick brown fox jumped over a tree and tripped over that tree the quick brown fox jumped over a tree and tripped over that tree the quick brown fox jumped over a tree and tripped over that tree the quick brown fox jumped over a tree and tripped over that tree"; 
-	const test1Arr = test1.split(' '); 
+	const test1 = "the quick brown fox jumped over a tree and tripped over that tree";
+	const test1Arr = test1.split(' ');
+	const tempStyles = []; 
+	for (var i = 0; i < test1Arr.length; ++i){
+		var letterStyles = []; 
+		for (var j = 0; j < test1Arr[i].length; ++j){
+			letterStyles.push(''); 
+		}
+		tempStyles.push(letterStyles); 
+	}
+	
+	const [styles, setStyles] = useState(tempStyles); 
+	
+	const [currWord, setWord] = useState(0); 
+	const [currChar, setChar] = useState(0); 
+	const [prevPos, setPrev] = useState(0); 
+
+	useEffect(() => {
+		const alterStyle = (e) => {
+			var temp = styles.slice(); 
+			if (e.code === 'Space'){ setWord(old => old + 1); setPrev(currChar); setChar(-1);}
+			else if (e.code === 'Backspace') {setWord(old => old - 1); setChar(prevPos - 1);}
+			else if (e.code === "Key" + test1Arr[currWord][currChar].toUpperCase()) temp[currWord][currChar] = 'correct'; 
+			else if (e.code !== "Key" + test1Arr[currWord][currChar].toUpperCase()) temp[currWord][currChar] = 'incorrect'; 
+			setStyles(temp); 
+			setChar(old => old + 1); 
+		}
+	
+		window.addEventListener("keydown", alterStyle);
+	
+		return () => window.removeEventListener("keydown", alterStyle);
+	  }, [currChar, currWord, styles, prevPos, test1Arr]);
+
 	return (
 		<div className='outer-word-container'>
 			<div className="word-container"> 	
-				{test1Arr.map(monoWord => <Word inputWord={monoWord}/> )}
-				<Word inputWord="Scoll"/>
+				{test1Arr.map((monoWord, idx) => <Word key={idx} inputWord = {monoWord} letterStyles={styles[idx]}/>)} 
 			</div>
 		</div>
 	)
--- src/word.js
+++ src/word.js
@@ -3,9 +3,11 @@ import './word.css'
 
 export default function Word(props) {
 	const letters = props.inputWord.split('');
+	const styles = props.letterStyles; 
+	// console.log(styles); 
 	return (
 		<div className="word">
-			{letters.map(char => <letter>{char}</letter>)}
+			{letters.map((char, idx)=> <letter key={idx} className={styles[idx]}>{char}</letter>)}
 		</div>
 	)
 }
