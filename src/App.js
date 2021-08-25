d30a0e03c57767eb71c74e432ec910d5bead3156
Kefan Cao <k33cao@uwaterloo.ca>
2021-08-24 21:20:01 -0400

    perfected control logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, {useState} from 'react'; 
 import Welcome from './Welcome';
+import Game from './Game'
 
 // App only returns Welcome Page so far
 function App() {
   const [game, setGame] = useState(false); 
   return (
-    {game ? <Welcome start={}/>}
+    <>
+      {!game ? <Welcome setStart={setGame} /> : <Game />}
+    </>
   );
 }
 
