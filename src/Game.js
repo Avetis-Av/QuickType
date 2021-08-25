f545161edeb41196d9d75ed33a4da973102f8233
Avetis Avagyan <avikavagyan2002@gmail.com>
2021-08-24 21:52:39 -0400

    Added timer component

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Timer from "./Timer.js"
 
 export default function Game() {
+
 	return (
 		<div>
-			Start Typing...
+			<Timer  initMin={0} initSec={10} />
+			<h1> Start Typing... </h1>
 		</div>
 	)
 }
