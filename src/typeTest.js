e850295375b55d8d31d4ded9fc83349d98edad79
Kefan Cao <k33cao@uwaterloo.ca>
2021-08-25 13:31:14 -0400

    Added container

--- src/typeTest.js
+++ src/typeTest.js
@@ -3,11 +3,14 @@ import Word from './word';
 import './typeTest.css'
 
 export default function TypeTest() {
-	const test1 = "the quick brown fox jumped over a tree and tripped over that tree"; 
+	const test1 = "the quick brown fox jumped over a tree and tripped over that tree the quick brown fox jumped over a tree and tripped over that tree the quick brown fox jumped over a tree and tripped over that tree the quick brown fox jumped over a tree and tripped over that tree"; 
 	const test1Arr = test1.split(' '); 
 	return (
-		<div className="word-container"> 	
-			{test1Arr.map(monoWord => <Word inputWord={monoWord}/> )}
+		<div className='outer-word-container'>
+			<div className="word-container"> 	
+				{test1Arr.map(monoWord => <Word inputWord={monoWord}/> )}
+				<Word inputWord="Scoll"/>
+			</div>
 		</div>
 	)
 }
