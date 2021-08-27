44a0b60ea2c7849b31db7aa395702f318f1707e4
Kefan Cao <k33cao@uwaterloo.ca>
2021-08-27 13:05:53 -0400

    Added words as a prop

--- src/Game.js
+++ src/Game.js
@@ -3,11 +3,11 @@ import Timer from "./Timer.js"
 import TypeTest from './typeTest';
 
 export default function Game({givenMin, givenSec, setStyles, setFin}) {
-
+	const sampleTest = "The quick brown foxes are big and fat and can't move fast enough to avoid being shot and killed, it really is a shame... Sign :("
 	return (
 		<div>
 			<Timer initMin={givenMin} initSec={givenSec} setFin={setFin} />
-			<TypeTest setStlLst={setStyles} />
+			<TypeTest setStlLst={setStyles} word={sampleTest}/>
 			<h1> Start Typing... </h1>
 		</div>
 	)
