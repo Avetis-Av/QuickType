bfb9e59082144f3242891bb3b84aa268ac66c3b8
Kefan Cao <k33cao@uwaterloo.ca>
2021-08-27 13:38:03 -0400

    Added more to the sample test str

--- src/Game.js
+++ src/Game.js
@@ -3,7 +3,7 @@ import Timer from "./Timer.js"
 import TypeTest from './typeTest';
 
 export default function Game({givenMin, givenSec, setStyles, setFin}) {
-	const sampleTest = "The quick brown foxes are big and fat and can't move fast enough to avoid being shot and killed, it really is a shame... Sign :("
+	const sampleTest = "The quick brown foxes are big and fat and can't move fast enough to avoid being shot and killed, it really is a shame... Sign :(, The quick brown foxes are big and fat and can't move fast enough to avoid being shot and killed The quick brown foxes are big and fat and can't move fast enough to avoid being shot and kille The quick brown foxes are big and fat and can't move fast enough to avoid being shot and kille The quick brown foxes are big and fat and can't move fast enough to avoid being shot and kille"
 	return (
 		<div>
 			<Timer initMin={givenMin} initSec={givenSec} setFin={setFin} />
