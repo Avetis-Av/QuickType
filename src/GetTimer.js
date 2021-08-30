83a9cde7aba979b2e5df13331cadd8e69e6850dd
Kefan Cao <k33cao@uwaterloo.ca>
2021-08-30 15:01:15 -0400

    Added stylish button for submit button

--- src/GetTimer.js
+++ src/GetTimer.js
@@ -6,13 +6,15 @@ export default function GetTimer(props) {
 
     return (
         <div className="TimeInp">
-            <h1> Enter The Time Limit </h1>
-
-            <input type="text" value={props.min} onChange={m => props.setM(m.target.value)} placeholder={"minutes"} />
-            <input type="text" value={props.sec} onChange={s => props.setS(s.target.value)} placeholder={"seconds"} />
-
-            <button onClick={() => props.setT(true) }> 
-                Enter 
+            <div className="input-prompt">
+                <h1> Enter The Time Limit </h1>
+                <div className="inputs"> 
+                    <input type="text" value={props.min} onChange={m => props.setM(m.target.value)} placeholder={"minutes"} />
+                    <input type="text" value={props.sec} onChange={s => props.setS(s.target.value)} placeholder={"seconds"} />
+                </div>
+            </div>
+            <button class="button-50" role="button" onClick={() => props.setT(true) }>  
+                Enter
             </button>
         </div>
     )
